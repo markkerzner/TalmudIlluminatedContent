3675d54c866dd2593d79714ad306ddafc3d6d5c2
Mark Kerzner <mark@elephantscale.com>
2016-08-21 09:29:33 -0500

    programming and talmud work

--- upload_rsync.sh
+++ upload_rsync.sh
@@ -1,1 +1,9 @@
-rsync -a -v  --exclude .git --exclude images ../TalmudIlluminated/ top8team@top8.biz:/home/top8team/public_html/talmudilluminated.com/
+# This relies on the ssh client to be permanently redirected to port 7822
+# as required by top8
+# To achieve this:
+# cat .ssh/config 
+# Host top8.biz
+#    Port 7822
+
+rsync -a -v  --exclude .git --exclude  upload_rsync.sh --exclude *~ --exclude images ../TalmudIlluminated/ top8team@top8.biz:/home/top8team/public_html/talmudilluminated.com/
+
