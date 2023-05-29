cbb3d4e5326ccf2bd1ac54bfb1fd4fc484c57989
Mark Kerzner <mark@elephantscale.com>
2023-05-29 15:00:22 -0500

    updates

--- deploy1.sh
+++ deploy1.sh
@@ -1,3 +1,5 @@
 scp  -P 7822 $1 top8team@top8.biz:/home/top8team/public_html/talmudilluminated.com/
 
 # scp  -P 7822 $1 top8team@top8.biz:/home/top8team/public_html/talmudilluminated.com/moed_katan/
+
+# scp  -P 7822 about.html top8team@top8.biz:/home/top8team/public_html/talmudilluminated.com/
