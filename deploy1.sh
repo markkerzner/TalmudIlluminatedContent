461c8086a2bbbc8fb41af6b2f01f9cf69e9150a9
Mark Kerzner <mark@elephantscale.com>
2023-08-15 21:23:35 -0500

    kid 3

--- deploy1.sh
+++ deploy1.sh
@@ -1,5 +1,5 @@
 # Update one page in the root directory
-scp  -P 7822 $1 top8team@top8.biz:/home/top8team/public_html/talmudilluminated.com/
+# scp  -P 7822 $1 top8team@top8.biz:/home/top8team/public_html/talmudilluminated.com/
 
-# Update one page (given as an argument) in the gittin directory
-# scp  -P 7822 $1 top8team@top8.biz:/home/top8team/public_html/talmudilluminated.com/gittin/
+# Update one page (given as an argument) in the kiddushin directory
+scp  -P 7822 $1 top8team@top8.biz:/home/top8team/public_html/talmudilluminated.com/kiddushin/
